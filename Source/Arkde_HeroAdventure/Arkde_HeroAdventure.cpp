b2ffb65a438590d78edfb1127d6c4cd0866cfb0a
Papas338 <daniel.r.97@hotmail.com>
2020-03-13 15:03:06 -0500

    Initial commit for my project.

---
+++ Source/Arkde_HeroAdventure/Arkde_HeroAdventure.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Arkde_HeroAdventure.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arkde_HeroAdventure, "Arkde_HeroAdventure" );
---
+++ Source/Arkde_HeroAdventure/Arkde_HeroAdventureGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Arkde_HeroAdventureGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKDE_HEROADVENTURE_API AArkde_HeroAdventureGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
